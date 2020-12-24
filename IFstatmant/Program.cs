d7c0840136fce5554d7375880428a866c834efb2
Erfan <marston873@gmail.com>
2020-12-24 22:00:30 +0330

    Add project files.

---
+++ IFstatmant/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IFstatmant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 85;
+
+            if (age <= 15)
+            {
+                Console.WriteLine("you can't use ");
+            }
+            if (age > 15)
+                    {
+                Console.WriteLine("you can use ");
+            }
+        }
+    }
+}
